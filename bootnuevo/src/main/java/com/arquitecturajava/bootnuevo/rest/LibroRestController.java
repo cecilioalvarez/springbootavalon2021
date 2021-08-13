4500e6efa7477d0e6532e0424f9787698f411feb
cecilioAlvarez <cmac@arquitecturajava.com>
2021-08-13 10:38:30 +0200

    libro rest controller

--- bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/rest/LibroRestController.java
+++ bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/rest/LibroRestController.java
@@ -2,7 +2,10 @@ package com.arquitecturajava.bootnuevo.rest;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +16,21 @@ import com.arquitecturajava.bootnuevo.servicios.LibroService;
 @RequestMapping("/webapi/libros")
 public class LibroRestController {
 	
+	@DeleteMapping
+	public void borrar(Libro libro) {
+		servicio.borrar(libro);
+	}
+	
+	@PutMapping
+	public void actualizar(Libro libro) {
+		servicio.actualizar(libro);
+	}
+	@PostMapping
+	public void insertar(Libro libro) {
+		servicio.insertar(libro);
+	}
+
+
 	private LibroService servicio;
 
 	public LibroRestController(LibroService servicio) {
