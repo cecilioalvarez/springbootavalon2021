73ad4baf28ee75d7f79a2dfa32d6ff0c10b77576
miguel <miguel.perez@juanxxiii.net>
2021-08-13 11:22:22 +0200

    Métodos Rest creados

--- bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/rest/LibroRestController.java
+++ bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/rest/LibroRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +44,13 @@ public class LibroRestController {
 	public void insertar(Libro libro) {
 		servicio.insertar(libro);
 	}
+
+	@GetMapping("/{isbn}")
+	public Libro buscarUno(@PathVariable String isbn) {
+		return servicio.buscarUno(isbn);
+	}
+	
+	
 
 	
 }
