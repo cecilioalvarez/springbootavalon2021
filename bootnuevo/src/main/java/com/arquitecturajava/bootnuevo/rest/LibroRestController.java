aadd0be4e68e119eb1cc58333bebbb074bb6aa05
RubenPS <rpozo1991@gmail.com>
2021-08-13 11:48:14 +0200

    RestController Libro

---
+++ bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/rest/LibroRestController.java
@@ -0,0 +1,45 @@
+package com.arquitecturajava.bootnuevo.rest;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.arquitecturajava.bootnuevo.negocio.Libro;
+import com.arquitecturajava.bootnuevo.servicios.LibroService;
+
+@RestController
+@RequestMapping("/webapi/libros")
+public class LibroRestController {
+	private LibroService servicio;
+
+	public LibroRestController(LibroService servicio) {
+		super();
+		this.servicio = servicio;
+	}
+	
+	@DeleteMapping 
+	public void borrar(Libro libro) {
+		servicio.borrar(libro);
+	}
+	
+	@PostMapping
+	public void insertar (Libro libro) {
+		servicio.insertar(libro);
+	}
+	
+	@GetMapping
+	public List<Libro> buscarTodos(){
+		return servicio.buscarTodos();
+	}
+	
+	@GetMapping("{isbn}")
+	public Libro buscarUno(@PathVariable String isbn) {
+		return servicio.buscarUno(isbn);
+	}
+	
+}
