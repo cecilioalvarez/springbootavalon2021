7eba0bdfffce0f91b396bbe729685c28fb596eaf
Germán Zunzunegui <gerzunzu@gmail.com>
2021-08-13 13:38:59 +0200

    N - Servicio REST de nivel 2 para libros y autores.

---
+++ boot/src/main/java/com/arquitecturajava/boot/rest/AuthorRestController.java
@@ -0,0 +1,28 @@
+package com.arquitecturajava.boot.rest;
+
+import com.arquitecturajava.boot.business.Author;
+import com.arquitecturajava.boot.services.LibraryService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/webapi/authors")
+class AuthorRestController {
+
+    @Autowired
+    private LibraryService LIBRARY_SERVICE;
+    
+    @GetMapping("/{pk_id}")
+    public Author getAuthor(@PathVariable String pk_id) {
+        return this.LIBRARY_SERVICE.select(new Author(pk_id));
+    }
+    
+    @GetMapping
+    public List<Author> getAllAuthors() {
+        return this.LIBRARY_SERVICE.selectAuthors();
+    }
+}
---
+++ boot/src/main/java/com/arquitecturajava/boot/rest/BookRestController.java
@@ -0,0 +1,51 @@
+package com.arquitecturajava.boot.rest;
+
+import com.arquitecturajava.boot.business.Book;
+import com.arquitecturajava.boot.services.LibraryService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/webapi/books")
+public class BookRestController {
+
+    @Autowired
+    private LibraryService LIBRARY_SERVICE;
+    
+    @GetMapping("/{pk_isbn}")
+    public Book getBook(@PathVariable String pk_isbn) {
+        return this.LIBRARY_SERVICE.selectBook(new Book(pk_isbn));
+    }
+    
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return this.LIBRARY_SERVICE.selectBooks();
+    }
+    
+    @PostMapping
+    public int insert(@RequestBody Book book) {
+        return this.LIBRARY_SERVICE.insert(book);
+    }
+    
+    @DeleteMapping("/{pk_isbn}")
+    public int delete(@PathVariable String pk_isbn) {
+        return this.LIBRARY_SERVICE.deleteBook(new Book(pk_isbn));
+    }
+    
+    @PutMapping("/{pk_isbn}")
+    public int update(@PathVariable String pk_isbn, @RequestBody Book book) {
+        Book bookToUpdate = this.LIBRARY_SERVICE.selectBook(new Book(pk_isbn));
+        bookToUpdate.setTitle(book.getTitle());
+        bookToUpdate.setFk_author(book.getFk_author());
+        bookToUpdate.setChapters(book.getChapters());
+        return this.LIBRARY_SERVICE.update(bookToUpdate);
+    }
+}
