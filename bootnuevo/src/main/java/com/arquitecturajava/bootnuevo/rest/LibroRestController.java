7361aa9ee9ab84be25f23c525cbc4a3b593885f6
cecilioAlvarez <cmac@arquitecturajava.com>
2021-08-13 10:14:40 +0200

    getmapping

--- bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/rest/LibroRestController.java
+++ bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/rest/LibroRestController.java
@@ -2,12 +2,15 @@ package com.arquitecturajava.bootnuevo.rest;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arquitecturajava.bootnuevo.negocio.Libro;
 import com.arquitecturajava.bootnuevo.servicios.LibroService;
 
 @RestController
+@RequestMapping("/webapi/libros")
 public class LibroRestController {
 	
 	private LibroService servicio;
@@ -16,7 +19,7 @@ public class LibroRestController {
 		super();
 		this.servicio = servicio;
 	}
-
+	@GetMapping
 	public List<Libro> buscarTodos() {
 		
 		return servicio.buscarTodos();
