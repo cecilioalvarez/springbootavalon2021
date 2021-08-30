a75ad3bf33d4874cb8405842d0d3efcb56f9a5e2
cecilioAlvarez <cmac@arquitecturajava.com>
2021-08-30 14:22:30 +0200

    command line runner

--- bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/BootnuevoApplication.java
+++ bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/BootnuevoApplication.java
@@ -1,13 +1,50 @@
 package com.arquitecturajava.bootnuevo;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.arquitecturajava.bootnuevo.negocio.Capitulo;
+import com.arquitecturajava.bootnuevo.negocio.Libro;
 
 @SpringBootApplication
-public class BootnuevoApplication {
+public class BootnuevoApplication implements CommandLineRunner {
+
+	@PersistenceContext
+	EntityManager em;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootnuevoApplication.class, args);
+	}
+
+	@Override
+	@Transactional
+	public void run(String... args) throws Exception {
+
+		TypedQuery<Libro> consulta = em.createQuery("select distinct l from Libro l left join fetch l.capitulos", Libro.class);
+
+		List<Libro> lista = consulta.getResultList();
+
+		for (Libro l : lista) {
+
+			System.out.println(l.getIsbn());
+			System.out.println(l.getTitulo());
+			System.out.println(l.getAutor());
+
+			
+			for (Capitulo c : l.getCapitulos()) {
+
+				System.out.println(c.getTitulo());
+			}
+		}
+
 	}
 
 }
