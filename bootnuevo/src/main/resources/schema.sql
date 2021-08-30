68d687637e4bf3ada7b6ceb69d2b8ddbc07f9604
miguel <miguel.perez@juanxxiii.net>
2021-08-30 11:17:42 +0200

    Pruebas Unitarias Completadas

--- bootnuevo/src/main/resources/schema.sql
+++ bootnuevo/src/main/resources/schema.sql
@@ -1,3 +1,2 @@
 DROP TABLE IF EXISTS Libros;
-create table Libros (isbn varchar(10), titulo varchar(25),autor varchar(25));DROP TABLE IF EXISTS Libros;
-create table Libros (isbn varchar(10), titulo varchar(25),autor varchar(25));
+create table Libros (isbn varchar(10), titulo varchar(25),autor varchar(25), primary key (isbn));
--- bootnuevo/src/test/java/com/arquitecturajava/bootnuevo/BootnuevoApplicationTests.java
+++ bootnuevo/src/test/java/com/arquitecturajava/bootnuevo/BootnuevoApplicationTests.java
@@ -1,9 +1,11 @@
 package com.arquitecturajava.bootnuevo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,6 +26,30 @@ class BootnuevoApplicationTests {
 		assertEquals( "1",libro.getIsbn());
 		assertEquals( "Cecilio",libro.getAutor());
 		assertEquals( "Java",libro.getTitulo());
+	}
+	
+	@Test
+	@Transactional
+	void insertarUnLibroTest() {
+		Libro libro = new Libro ("4","python","Ana");
+		//Persist Lanza el insert del libro
+		em.persist(libro);
+		
+		Libro libro2 = em.find(Libro.class, "4");
+		assertEquals( "4",libro2.getIsbn());
+		assertEquals( "Ana",libro2.getAutor());
+		assertEquals( "python",libro2.getTitulo());
+	}
+	
+	@Test
+	@Transactional
+	void borrarUnLibroTest() {
+		Libro libro = em.find(Libro.class, "1");
+		
+		em.remove(libro);
+		
+		Libro libroVacio = em.find(Libro.class, "1");
+		assertNull(libroVacio);
 	}
 
 }
