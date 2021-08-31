e0a175d4026cf9d59da8b8f630bc50e45d9594c2
cecilioAlvarez <cmac@arquitecturajava.com>
2021-08-31 10:51:25 +0200

    jpa repo

--- bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/repositorios/jjpa/LibroRepositoryJPA.java
+++ bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/repositorios/jjpa/LibroRepositoryJPA.java
@@ -2,58 +2,68 @@ package com.arquitecturajava.bootnuevo.repositorios.jjpa;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 import com.arquitecturajava.bootnuevo.negocio.Capitulo;
 import com.arquitecturajava.bootnuevo.negocio.Libro;
 import com.arquitecturajava.bootnuevo.repositorios.LibroRepository;
 
 public class LibroRepositoryJPA implements LibroRepository {
 
+	@PersistenceContext
+	EntityManager em;
+	
 	@Override
 	public void actualizar(Libro libro) {
-		// TODO Auto-generated method stub
+		em.merge(libro);
 		
 	}
-
 	@Override
 	public void insertar(Libro libro) {
-		// TODO Auto-generated method stub
+		em.persist(libro);
 		
 	}
-
 	@Override
 	public void borrar(Libro libro) {
-		// TODO Auto-generated method stub
+		em.remove(em.merge(libro));	
 		
 	}
-
 	@Override
 	public List<Libro> buscarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return em.createQuery("select l from Libro l",Libro.class).getResultList();
 	}
 
 	@Override
 	public List<Libro> buscarTodosConCapitulos() {
 		// TODO Auto-generated method stub
-		return null;
+		 	return em.createQuery("select l from Libro l join fetch l.capitulos ",Libro.class).getResultList();
 	}
 
 	@Override
 	public List<Libro> buscarTituloyAutor(String titulo, String autor) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		TypedQuery<Libro> consulta=em.createQuery("select l from Libro l where l.autor=:autor and l.titulo=:titulo",Libro.class);
+		consulta.setParameter("titulo", titulo);
+		consulta.setParameter("autor", autor);
+		return consulta.getResultList();
 	}
 
 	@Override
 	public Libro buscarUno(String isbn) {
 		// TODO Auto-generated method stub
-		return null;
+		return em.find(Libro.class, isbn);
 	}
 
 	@Override
 	public List<Capitulo> buscarTodosCapitulos(Libro libro) {
 		// TODO Auto-generated method stub
-		return null;
+		TypedQuery<Capitulo> consulta= em.createQuery("select c from Capitulo c where c.libro.isbn=:isbn",Capitulo.class);
+		consulta.setParameter("isbn", libro.getIsbn());
+		return consulta.getResultList();
+		
 	}
 
 }
