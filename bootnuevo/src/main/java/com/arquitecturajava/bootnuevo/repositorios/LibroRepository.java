5c6ed07959c1d85a05954b49649c75299ad6d399
cecilioAlvarez <cmac@arquitecturajava.com>
2021-09-02 14:07:59 +0200

    ultimos spring data

--- bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/repositorios/LibroRepository.java
+++ bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/repositorios/LibroRepository.java
@@ -3,6 +3,7 @@ package com.arquitecturajava.bootnuevo.repositorios;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.arquitecturajava.bootnuevo.negocio.Capitulo;
 import com.arquitecturajava.bootnuevo.negocio.Libro;
@@ -10,11 +11,10 @@ import com.arquitecturajava.bootnuevo.negocio.Libro;
 public interface LibroRepository extends JpaRepository<Libro,String>{
 
 	
-	/*
-	List<Libro> buscarTodosConCapitulos();
-
-	List<Libro> buscarTituloyAutor(String titulo, String autor);
+	 List<Libro> buscarTodosConCapitulos();
 	
-	List<Capitulo> buscarTodosCapitulos(Libro libro);
-*/
+	@Query("select c from Capitulo c where c.libro.isbn=:isbn")
+	 List<Capitulo> buscarTodosCapitulos(String isbn);
+	 List<Libro> findByTituloAndAutor(String titulo,String autor);
+
 }
--- bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/servicios/standard/LibroServiceStandard.java
+++ bootnuevo/src/main/java/com/arquitecturajava/bootnuevo/servicios/standard/LibroServiceStandard.java
@@ -44,13 +44,12 @@ public class LibroServiceStandard implements LibroService  {
 	}
 
 	public List<Libro> buscarTodosConCapitulos() {
-		//return repositorio.buscarTodosConCapitulos();
-		return null;
+		return repositorio.buscarTodosConCapitulos();
+		
 	}
 
 	public List<Libro> buscarTituloyAutor(String titulo, String autor) {
-		//return repositorio.buscarTituloyAutor(titulo, autor);
-		return null;
+		return repositorio.findByTituloAndAutor(titulo, autor);
 	}
 
 	public Optional<Libro> buscarUno(String isbn) {
@@ -80,8 +79,8 @@ public class LibroServiceStandard implements LibroService  {
 	@Override
 	public List<Capitulo> buscarTodosCapitulos(Libro libro) {
 		
-		//return repositorio.buscarTodosCapitulos(libro);
-		return null;
+		return repositorio.buscarTodosCapitulos(libro.getIsbn());
+	
 	}
 
 	@Override
