820d31d0ed14ede0683f168b1f3447e0b11ba183
cecilioAlvarez <cmac@arquitecturajava.com>
2021-09-09 09:16:06 +0200

    scripts

--- bootnuevo/src/main/resources/data.sql
+++ bootnuevo/src/main/resources/data.sql
@@ -1,9 +1,20 @@
 insert into Libros values ("1","Java","Cecilio","editorial1");
+
 insert into Libros values ("2","Net","Pedro","editorial2");
-insert into Libros values ("3","PHP","Cecilio","editorial3");
+insert into Libros values ("3","Java EE","Cecilio","editorial3");
+insert into Libros values ("4","Java Avanzado","Cecilio","editorial4");
+insert into Libros values ("5","PHP","Cecilio","editorial5");
+insert into Libros values ("6","Java2","Cecilio","editorial6");
+insert into Libros values ("7","Java5","Cecilio","editorial7");
+insert into Libros values ("8","Java EE 7","Cecilio","editorial8");
 insert into Capitulos values ("intro",25,"1");
 insert into Capitulos values ("basico",20,"1");
 insert into Capitulos values ("introduccion",25,"2");
 insert into Editoriales values ("editorial1","categoriaA");
 insert into Editoriales values ("editorial2","categoriaB");
 insert into Editoriales values ("editorial3","categoriaC");
+insert into Editoriales values ("editorial4","categoriaC");
+insert into Editoriales values ("editorial5","categoriaC");
+insert into Editoriales values ("editorial6","categoriaC");
+insert into Editoriales values ("editorial7","categoriaC");
+insert into Editoriales values ("editorial8","categoriaC");
